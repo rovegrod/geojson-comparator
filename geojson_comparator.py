7d385bb9b4ef7626c662621899107456ed0473dc
Roberto Vega <roberto.vega@nexiona.com>
2020-04-15 09:06:46 +0200

    Increase precision with Numpy

--- geojson_comparator.py
+++ geojson_comparator.py
@@ -30,16 +30,29 @@ def geo_comparator(path_originalfile,path_file2check):
     origin_coords = origin["features"][0]["geometry"]["coordinates"]
     data_coords = data["features"][0]["geometry"]["coordinates"]
     
-    sum_origin = np.sum(origin_coords, axis=0)
-    sum_data = np.sum(data_coords, axis=0)
+    lister = lambda l: [coord for point in l for coord in point]
+    origin_list = lister(origin_coords)
+    data_list = lister(data_coords)
+    
+    for point in data_coords:
+        absolute_axis_error = np.sum(abs(np.subtract(origin_coords, data_coords)), axis=0)
 
-    absolute_error = abs(sum(np.subtract(origin_coords, data_coords)))
-    relative_error = np.divide(absolute_error, sum_origin) * 100
+    origin_sum = np.sum(origin_coords, axis=0)
+    data_sum = np.sum(data_coords, axis=0)
+    
+    relative_axis_error = np.divide(absolute_axis_error, origin_sum) * 100
+    absolute_mean_error_axis = np.divide(absolute_axis_error, len(origin_coords))
+    
+    absolute_error = sum(abs(np.subtract(origin_list, data_list)))
+    relative_error = (absolute_error / np.sum(origin_list)) * 100
+    absolute_mean_error = absolute_error / len(origin_list)
 
-    logging.info(f"Sum origin: {sum_origin}")
-    logging.info(f"Sum input: {sum_data}")
-    logging.info(f"Absolute error by axis: {absolute_error}")
-    logging.info(f"Relative error by axis: {relative_error}%")
+    logging.info(f"Absolute error by axis: {absolute_axis_error}")
+    logging.info(f"Mean absolute error by axis: {absolute_mean_error_axis}")
+    logging.info(f"Relative error by axis: {relative_axis_error}%\n")
+    logging.info(f"Absolute error: {absolute_error}")
+    logging.info(f"Mean absolute error: {absolute_mean_error}")
+    logging.info(f"Relative error: {relative_error}%")
 
 
 if __name__ == "__main__":
